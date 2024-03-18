ea2331ef230a5ac8368ea78d509a16c58e018b00
aryamvk <aryamvk099@gmail.com>
2024-03-19 00:29:31 +0530

    Add Python swip code

---
+++ DEF.py
@@ -0,0 +1,10 @@
+Num1=float(input("enter the first number"))
+Num2=float(input("enter the second number"))
+
+print("orignal value  Num1==Num1, Num2==Num2")
+
+temp = Num1
+Num1 = Num2
+Num2 = temp
+
+print ("After Swaping Num1={Num1} , Num2={Num2}")
